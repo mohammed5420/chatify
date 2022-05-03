c4c0df6a8e863abde0e2b5c649450260428c9ba4
mosalah <almoaz065@gmail.com>
2022-05-03 19:33:16 +0200

    fix: 🐛 Default RTL message body, Same User name text color

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -13,11 +13,21 @@ const Message = ({ message, user }) => {
         </div>
       </div>
 
-      <div className="space-y-1 max-w-md w-full">
-        <p className="text-md text-primary">{message.senderID.userName}</p>
+      <div className={`space-y-1 max-w-md w-full `}>
+        <p
+          className={`text-md  ${
+            message.senderID.uid === user.sub
+              ? 'text-primary'
+              : 'text-secondary'
+          }`}
+        >
+          {message.senderID.userName}
+        </p>
         <div className="max-w-fit">
           <div className="message bg-base-300 px-3 py-2 rounded-md ">
-            <p className="text-md text-slate-100">{message.content}</p>
+            <p dir="ltr" className="text-md text-slate-100">
+              {message.content}
+            </p>
           </div>
           <p dir="ltr" className="text-slate-400 w-full text-right">
             {moment(message.createdAt).fromNow()}
