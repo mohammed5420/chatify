2126ee9286f322d30d384e0102847894eb774eb2
mohammed5420 <mohamed.elbadwi.dev@gmail.com>
2023-03-16 09:28:10 +0200

    fixed wrong path import, added error message for createing new users

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,6 +1,6 @@
 const { v4: uuid } = require("uuid");
 const { z } = require("zod");
-const Room = require("../model/Room");
+const User = require("../model/User");
 
 const userValidationSchema = z.object({
   userName: z.string().min(3).max(50),
@@ -8,14 +8,17 @@ const userValidationSchema = z.object({
 
 const createUser = async (req, res) => {
   const { userName } = userValidationSchema.parse(req.body);
+  try {
+    const user = await User.create({
+      uid: uuid(),
+      userName,
+      picture: `https://api.dicebear.com/5.x/identicon/svg?seed=${userName}`,
+    });
 
-  const user = await User.create({
-    uid: uuid(),
-    userName,
-    picture: `https://api.dicebear.com/5.x/identicon/svg?seed=${userName}`,
-  });
-
-  return res.json({ user });
+    return res.json({ user });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
